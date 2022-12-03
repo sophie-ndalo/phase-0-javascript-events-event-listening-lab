fa653db6a680eaa2a12dad29b6c3319935dcc488
sophia <sophiendalo@gmail.com>
2022-12-03 14:18:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-function addingEventListener() {
+ document .getElementById('button').addEventListener("click",function() {
+
+ alert("I was clciked!");
+
+});
+let input = document.getElementById('button');
+
+function addingEventListener( clickAlert ) {
+  alert('I was clicked!');
+input.addEventListener('input', addingEventListener);
 }
